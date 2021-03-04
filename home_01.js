099aa007adec5d1f5d9cdb574597733398b74077
ljj960304 <79923045+ljj960304@users.noreply.github.com>
2021-03-04 13:30:29 +0900

    Add files via upload

---
+++ home_01.js
@@ -0,0 +1,19 @@
+function over1(obj) {
+    obj.src="image/국밥1.PNG";
+}
+function out1(obj) {
+    obj.src="image/국밥0.PNG";
+}
+function click1(obj) {
+    obj.src="image/국밥2.PNG";
+}
+
+function over2(obj) {
+    obj.src="image/국밥3.PNG";
+}
+function out2(obj) {
+    obj.src="image/국밥4.PNG";
+}
+function click2(obj) {
+    obj.src="image/국밥2.PNG";
+}
